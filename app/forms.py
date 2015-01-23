232acb2ca3fa90421c73ea4a845234283b878ca5
Saurabh Kumar <saurabhisalways4u@gmail.com>
2015-01-24 01:29:07 +0530

    Styled the index page a LOT.

--- app/forms.py
+++ app/forms.py
@@ -2,15 +2,19 @@ from django import forms
 from app.models import Thread, Post
 
 class ThreadForm(forms.ModelForm):
-	title = forms.CharField(max_length = 200)
-	text = forms.CharField(max_length = 4962)
+	title = forms.CharField(widget = forms.TextInput(attrs={
+		'placeholder': 'Your title here'
+		}), max_length = 200)
+	text = forms.CharField(widget = forms.Textarea(attrs={
+		'placeholder':'Enter your fucking text here...'
+		}), max_length = 4962)
 
 	class Meta:
 		model = Thread
 		fields = ['title', 'text']
 
 class PostForm(forms.ModelForm):
-	text = forms.CharField(max_length = 4962, )
+	text = forms.CharField(widget = forms.Textarea, max_length = 4962)
 
 	class Meta:
 		model = Post
