4b4cb6ada5861fe9996319ed449a365f88e30501
Param Singh <paramsingh258@gmail.com>
2015-01-28 15:43:59 +0530

    Renamed the create view to index and added redirection wrt posts.
    Since the create view had begun acting like the home page, renamed
    it.
    Added redirection to threads from urls like /app/post/45.
    Also, added some documentation to views.py.

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, url
 from app import views
 
 urlpatterns = patterns('',
-	url(r'^index/$', views.create, name="create_thread"),
+	url(r'^index/$', views.index, name="create_thread"),
 	url(r'^thread/(?P<thread_id>\w+)/$', views.thread, name="view thread"),
 	url(r'^numposts/(?P<thread_id>\w+)/$', views.numberofposts, name="numberofposts"),
 	url(r'^test/$', views.test, name="test"),
 	url(r'^report_thread/(?P<thread_id>\w+)/$', views.report_thread, name="report_thread"),
-	url(r'^report_post/(?P<post_id>\w+)/$', views.report_post, name="report_post")
+	url(r'^report_post/(?P<post_id>\w+)/$', views.report_post, name="report_post"),
+	url(r'^post/(?P<post_id>\w+)/$', views.post_redirect, name="post_redirect")
 )
--- app/views.py
+++ app/views.py
@@ -9,7 +9,9 @@ def threads_in_order():
 def threads_in_rev_order():
 	return sorted(Thread.objects.all(), key=lambda x: x.last_post_time)
 
-def create(request):
+def index(request):
+	''' A view for the index page. We list all the current threads here and 
+		also give the user the ability to create new threads here. '''
 	context_dict = {'form': ThreadForm()}
 	max_number = 20
 	if request.method == 'POST':
@@ -31,6 +33,7 @@ def create(request):
 	return render(request, 'app/index.html', context_dict)
 
 def thread(request, thread_id):
+	''' A view for rendering the threads '''
 	thread_id = int(thread_id)
 	try:
 		current_thread = Thread.objects.get(id=thread_id)
@@ -60,6 +63,9 @@ def thread(request, thread_id):
 	return render(request, 'app/thread.html', context_dict)
 
 def numberofposts(request, thread_id):
+	''' This view returns the posts in the thread in the form of JSON. 
+		It exists because eventually we might use Ajax to make requests to this 
+		view for autorefresh of threads. '''
 	thread_id = int(thread_id)
 	try:
 		current_thread = Thread.objects.get(id=thread_id)
@@ -72,23 +78,14 @@ def numberofposts(request, thread_id):
 
 	return JsonResponse(dict_posts)
 
-def test(request):
-	return render(request, 'app/test.html', {})
-
-
-def all_threads(request):
-	threads = threads_in_order()
-	context_dict = {}
-	context_dict['threads'] = threads
-	return render(request, 'app/index.html', context_dict)
-
 def report_thread(request, thread_id):
+	''' A view for the reporting of threads. '''
 	thread_id = int(thread_id)
 	context_dict = {'form': ThreadReportForm(), 'current_thread': thread_id}
 	try:
 		current_thread = Thread.objects.get(pk=thread_id)
 	except Thread.DoesNotExist:
-		return HttpResponse("The thread you are trying to report does not exist")
+		return HttpResponse("404 The thread you are trying to report does not exist")
 	if request.method == 'POST':
 		thread_report_form = ThreadReportForm(request.POST)
 		if thread_report_form.is_valid():
@@ -103,12 +100,13 @@ def report_thread(request, thread_id):
 	return render(request, 'app/thread_report.html', context_dict)
 
 def report_post(request, post_id):
+	''' A view for the reporting of posts. '''
 	post_id = int(post_id)
 	context_dict = {'form': PostReportForm(),'post_id': post_id}
 	try:
 		current_post = Post.objects.get(pk=post_id)
 	except Post.DoesNotExist:
-		return HttpResponse("The post you're trying to report does not exist")
+		return HttpResponse("404 The post you're trying to report does not exist")
 	if request.method == 'POST':
 		post_report_form = PostReportForm(request.POST)
 		if post_report_form.is_valid():
@@ -122,5 +120,21 @@ def report_post(request, post_id):
 		else:
 			print(post_report_form.errors)
 	return render(request, 'app/post_report.html', context_dict)
+
+def post_redirect(request, post_id):
+	''' A view for redirecting "/app/post/<post_id>" urls to appropriate threads.'''
+	post_id = int(post_id)
+	try:
+		current_post = Post.objects.get(pk=post_id)
+		thread = current_post.thread
+		id = thread.id
+		try:
+			Thread.objects.get(pk=id)
+		except Thread.DoesNotExist:
+			return HttpResponse("404 Does Not Exist")
+		return HttpResponseRedirect("/app/thread/"+str(id)+"/#divPost"+str(post_id))
+	except Post.DoesNotExist:
+		return HttpResponse("404 The post you're trying to open does not exist")
+
 
 
