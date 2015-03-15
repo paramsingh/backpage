f2d30c88f1742d841a9a2b96a87bd9b865336644
Param Singh <paramsingh258@gmail.com>
2015-03-15 18:01:07 +0530

    Fix urls.py to remove test view.

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns('',
 	url(r'^index/$', views.index, name="create_thread"),
 	url(r'^thread/(?P<thread_id>\w+)/$', views.thread, name="view thread"),
 	url(r'^numposts/(?P<thread_id>\w+)/$', views.numberofposts, name="numberofposts"),
-	url(r'^test/$', views.test, name="test"),
 	url(r'^report_thread/(?P<thread_id>\w+)/$', views.report_thread, name="report_thread"),
 	url(r'^report_post/(?P<post_id>\w+)/$', views.report_post, name="report_post"),
 	url(r'^post/(?P<post_id>\w+)/$', views.post_redirect, name="post_redirect")
